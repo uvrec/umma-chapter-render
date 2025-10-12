fefeb7a2f2f367686ceb0e3d0cbcca60077c733b
uvrec <168590891+uvrec@users.noreply.github.com>
2025-10-12 15:14:40 +0200

    Create index.ts

---
+++ src/components/GlobalAudioPlayer/index.ts
@@ -0,0 +1,60 @@
+// index.ts - Центральний файл експортів для GlobalAudioPlayer
+
+// Основні компоненти та хуки
+export { AudioProvider, GlobalAudioPlayer, useAudio } from './GlobalAudioPlayer';
+
+// TypeScript типи
+export type {
+  Track,
+  RepeatMode,
+  AudioContextType,
+  GlobalAudioPlayerProps,
+  AudioProviderProps,
+} from './GlobalAudioPlayer.types';
+
+// Утиліти
+export {
+  formatTime,
+  parseTime,
+  isMediaSessionSupported,
+  isWakeLockSupported,
+  updateMediaSession,
+  savePlaylist,
+  loadPlaylist,
+  saveVolume,
+  loadVolume,
+  isValidAudioUrl,
+  generateTrackId,
+  getFileNameFromUrl,
+  fileToTrack,
+} from './GlobalAudioPlayer.utils';
+
+// Кастомні хуки
+export {
+  usePlaybackProgress,
+  useVolumeControl,
+  useRepeatMode,
+  usePlaylistNavigation,
+  usePlaylistPersistence,
+  usePlaybackHistory,
+  useLoadingState,
+  usePlayerKeyboardShortcuts,
+  usePlaybackRate,
+  useShufflePlaylist,
+  usePlaylistFilter,
+  useSleepTimer,
+} from './GlobalAudioPlayer.hooks';
+
+// Supabase адаптер
+export {
+  convertSupabaseTrackToPlayerTrack,
+  loadTrackFromSupabase,
+  loadPlaylistTracks,
+  loadRecentTracks,
+  trackPlayEvent,
+  useSupabasePlaylist,
+  useAudioTracking,
+} from './GlobalAudioPlayer.supabase-adapter';
+
+// CSS стилі (імпортуйте окремо там де потрібно)
+// import '@/components/GlobalAudioPlayer/GlobalAudioPlayer.css';
